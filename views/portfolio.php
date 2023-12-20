be0f5c0e521b3a2ac042c07535f97ef99e29309a
gunetang14 <quirozangel1991@gmail.com>
2023-12-20 18:06:57 -0400

    nuevos proyectos

--- views/portfolio.php
+++ views/portfolio.php
@@ -70,6 +70,56 @@
                 </ul>
             </div>
         </div>
+
+        <div class="page">
+            <a class="page__enlace" href="https://salon-angelquiroz.000webhostapp.com/" target="_blank">
+                <picture class="page__centrado"> 
+                <source srcset="build/img/appsalon.webp" type="image/webp">
+                <source srcset="build/img/appsalon.png" type="image/png">
+                <img class="page__imagen" 
+                    loading="lazy" 
+                    src="build/img/appsalon.jpg" 
+                    alt="imagen pagina">
+                </picture>
+            </a>
+            <p class="page__nombre">AppSalon</p>
+            <p class="page__descripcion">Administrador y Organizador de Citas de un Salon de Belleza. Contiene MVC, CRUD</p>
+            <div class="page__tecnologias">
+                <p class="page__nombre-lenguajes">Lenguajes, Frameworks y Librerias</p>
+                <ul class="page__tecnologia">
+                    <li class="page__tecnologia-nombre">PHP</li>
+                    <li class="page__tecnologia-nombre">SASS</li>
+                    <li class="page__tecnologia-nombre">JavaScript</li>
+                    <li class="page__tecnologia-nombre">SQL</li>
+                    <li class="page__tecnologia-nombre">Brevo</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="page">
+            <a class="page__enlace" href="https://bienesraices-angelquiroz.000webhostapp.com/" target="_blank">
+                <picture class="page__centrado"> 
+                <source srcset="build/img/bienesraices.webp" type="image/webp">
+                <source srcset="build/img/bienesraices.png" type="image/png">
+                <img class="page__imagen" 
+                    loading="lazy" 
+                    src="build/img/bienesraices.jpg" 
+                    alt="imagen pagina">
+                </picture>
+            </a>
+            <p class="page__nombre">BienesRaices</p>
+            <p class="page__descripcion">Página sobre venta de casas y apartamentos. Contiene MVC, CRUD</p>
+            <div class="page__tecnologias">
+                <p class="page__nombre-lenguajes">Lenguajes, Frameworks y Librerias</p>
+                <ul class="page__tecnologia">
+                    <li class="page__tecnologia-nombre">PHP</li>
+                    <li class="page__tecnologia-nombre">SASS</li>
+                    <li class="page__tecnologia-nombre">JavaScript</li>
+                    <li class="page__tecnologia-nombre">SQL</li>
+                    <li class="page__tecnologia-nombre">Brevo</li>
+                </ul>
+            </div>
+        </div>
     </div>
 
 </div>
